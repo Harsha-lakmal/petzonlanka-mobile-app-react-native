933b24dcf0ddf234ba1d8017cead5700b3c3ff12
Harsha-lakmal <lakmalharsha424@gmail.com>
2025-05-11 15:24:06 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+function App() {
+
+  
+
+  const safePadding = '5%';
+
+  return (
+    <View>
+      <Text>I am Harsha Lakmal .  this is petZon-lanka-moblie app </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
